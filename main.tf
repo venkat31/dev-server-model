e6ab55325021db8ccb5834ea6bd9afe93ca031c6
venkatbhattiprolu <vbhat847@gmail.com>
2023-02-28 18:52:27 -0800

    using terraform vpc existing module

--- main.tf
+++ main.tf
@@ -1,31 +1,28 @@
-provider "aws"{
+provider "aws"{}
 
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_vpc" "myapp-vpc"{
-  cidr_block = var.vpc_cidr_block 
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block 
+  azs             = [var.avail_zone]
+  public_subnets  = [var.subnet_cidr_block]
+  public_subnet_tags ={ Name = "${var.env_prefix}-subnet-1"} 
+  
   tags = {
-    Name = "${var.env_prefix}-vpc"
+    Terraform = "true"
+    Environment = "${var.env_prefix}-vpc"
   }
-}
-
-module "myapp-subnet" {
-  source = "./modules/subnet"
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
-  vpc_id = aws_vpc.myapp-vpc.id
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
 module "myapp-webserver" {
   source = "./modules/webserver"
-  vpc_id = aws_vpc.myapp-vpc.id
-  subnet_id = module.myapp-subnet.subnet.id
+  vpc_id = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnets[0]
   my_ip = var.my_ip
   env_prefix  = var.env_prefix
   public_key_location = var.public_key_location
   instance_type = var.instance_type
   avail_zone = var.avail_zone
-  image = "amzn2-ami-hvm-*-x86_64-gp2"
+  image = var.image
 }
